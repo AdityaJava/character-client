916bf2a42a840464f872e1d215fb381e9a1934dc
Aditya <adityadudhal97@gmail.com>
2023-07-07 21:31:37 +0530

    paging

---
+++ src/components/character/ShowAllCharacters.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ShowAllCHaracters extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+
+}
--- src/components/character/api/CharacterService.js
+++ src/components/character/api/CharacterService.js
@@ -1,18 +1,21 @@
 import axios from "axios";
 
-class CharacterService{
-    retrieveAllHousesService(houseName){
+class CharacterService {
+    retrieveAllHousesService(houseName) {
         return axios.get('http://localhost:8080/api/v1/characters/houses');
     }
 
-    getCharacterById(characterId){
+    getCharacterById(characterId) {
         console.log("getCharacterById")
         console.log(characterId)
         return axios.get(`http://localhost:8080/api/v1/characters/${characterId}`);
     }
-    
-    retrieveFamilyTree(houseName){
+
+    retrieveFamilyTree(houseName) {
         return axios.get(`http://localhost:8080/api/v1/characters/familytree/${houseName}`)
+    }
+    getAllCHaracters(pageNumber) {
+        return axios.get(`http://localhost:8080/api/v1/characters?page=${pageNumber}&size=30`)
     }
 }
 
